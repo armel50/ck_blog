ce24b19e2d0862dca73ec60226bfad5788d0c91e
Armel <armelfli50@gmail.com>
2020-05-24 20:50:14 -0500

    added redux to the app

---
+++ src/reducer/MainReducer.js
@@ -0,0 +1,17 @@
+import { createStore } from "redux";
+
+const reducer = (state=[],action) =>{
+    switch (action.type) {
+        case 'ADDPOST':
+          return [...state, action.payload]
+        case 'DELETEPOST':
+          return [...state].filter(e => e.id !== action.payload)
+        case 'EDITPOST':
+            let post = state.find(e => e.id == action.payload.id)
+            post = action.payload
+            return [...state, post]
+        default:
+          return state
+      }
+}
+const Store = createStore(reducer)
